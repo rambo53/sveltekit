648ee25d109ae35443fe850fecb3bf8cddd728a0
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-15 14:26:51 +0100

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,17 @@
 <script>
 
-    fetch('http://127.0.0.1:5000/test',{
-      method: 'GET',
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+import { env as public_env} from '$env/dynamic/public'
+
+let message = "";
+
+fetch(public_env.PUBLIC_URL_API+'test',{
+  method: 'GET',
+})
+  .then((response) => response.json())
+  .then((data) => message = data.message);
     
-    </script>
+</script>
 
 <h1>about</h1>
+
+<p>{message}</p>
