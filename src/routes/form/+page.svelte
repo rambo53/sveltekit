10af3c5c9f379a89d971ce3255e6c6c41afbe247
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-16 13:59:54 +0100

    Update +page.svelte

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,1 +1,14 @@
+<script>
+    export let form;
+</script>
+
 <h1>form</h1>
+
+<form method="post" action="?/send">
+    <label for="file">fichier :</label>
+    <br>
+    <input name="file" type="file">
+    {#if form?.error}<span>{form?.message}</span>{/if}
+    <br>
+    <button>send file</button>
+</form>
