22f8585d782efc235de1ff50011b09442416d86f
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-18 14:49:03 +0100

    Update +page.svelte

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,14 +1,26 @@
 <script>
-    export let form;
+    export let data;
+    import Button from '../../lib/button.svelte';
+    import {test} from './form.js'
+    
+    let texte = test()
+    
+    const { configs } = data;
 </script>
 
 <h1>form</h1>
+
+<Button message="yolo depuis form!"/>
+
+<h2>{texte}</h2>
 
 <form method="post" action="?/send">
     <label for="file">fichier :</label>
     <br>
     <input name="file" type="file">
-    {#if form?.error}<span>{form?.message}</span>{/if}
     <br>
+    {#each configs as config}
+        <p>{ config.name }</p>
+    {/each}
     <button>send file</button>
 </form>
