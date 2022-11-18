1d23a74e4a301e79fa64956e6c72c3d435f24362
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-18 14:50:27 +0100

    Add files via upload

---
+++ src/lib/button.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Swal from "sweetalert2";
+    export let message;
+
+    function showModal(){
+        Swal.fire(message);
+    }
+
+</script>
+
+<style>
+    .bg{
+        background-color: aqua;
+    }
+</style>
+
+<button class="bg" on:click={showModal}>Go</button>
