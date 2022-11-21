88d4f3898d7f50afb2280840e69daf235f2cac36
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-21 17:47:07 +0100

    Update +page.server.js

--- src/routes/authenticate/+page.server.js
+++ src/routes/authenticate/+page.server.js
@@ -6,9 +6,22 @@ export const load = async () => {
 }
 
 export const actions = {
-    auth: async ({ request }) =>{
+    auth: async ({ request}) =>{
         const formdata = await request.formData();
-        console.log(formdata)
+        console.log(formdata);
+        const user = {
+            id :formdata.get("id"),
+            password:formdata.get("password")
+        }
+        console.log(JSON.stringify(user));
+        fetch(public_env.PUBLIC_URL_API+"authenticate", {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers:{
+                "Content-type":"application/json; charset=UTF-8",
+                "Accept":"application/json"
+            }
+        })
         throw redirect(307, '/');
     }
 }
