22882702ff7e17d695fd233a2a3d744d4c04c6dd
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-21 11:44:49 +0100

    Add files via upload

---
+++ src/authenticate/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Authentification</h1>
+
+<form id="formAuth" method="post" action="?/auth">
+    <label for="id">identifiant :</label>
+    <br>
+    <input name="id" type="texte">
+    <br>
+    <label for="id">password :</label>
+    <br>
+    <input name="password" type="texte">
+    <br>
+
+    <button>Go</button>
+</form>
