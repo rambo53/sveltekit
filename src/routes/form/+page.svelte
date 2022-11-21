3423645616d69d142cfcf13e01b95885453bed34
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-21 09:39:07 +0100

    Update +page.svelte

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { redirect } from '@sveltejs/kit';
+    import { env as public_env} from '$env/dynamic/public'
     export let data;
     import Button from '../../lib/button.svelte';
     import {test} from './form.js'
@@ -6,6 +8,20 @@
     let texte = test()
     
     const { configs } = data;
+
+    let files;
+
+    const checkForm = async () => {
+        const form = document.getElementById('form');
+        let formdata = new FormData(form);
+        
+        let r = await fetch(public_env.PUBLIC_URL_API+'file', {method: "POST", body: formdata});
+        let data = await r.json();
+        if(data.status==200){
+            form.reset();
+        }
+    }
+
 </script>
 
 <h1>form</h1>
@@ -14,10 +30,10 @@
 
 <h2>{texte}</h2>
 
-<form method="post" action="?/send">
+<form id="form" method="post" on:submit|preventDefault={checkForm}>
     <label for="file">fichier :</label>
     <br>
-    <input name="file" type="file">
+    <input name="file" type="file" bind:files>
     <br>
     {#each configs as config}
         <p>{ config.name }</p>
