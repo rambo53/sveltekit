f4e2794c627317d49b85cce007380b4d057ab666
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-22 11:55:34 +0100

    Update +page.server.js

--- src/routes/authenticate/+page.server.js
+++ src/routes/authenticate/+page.server.js
@@ -1,10 +1,6 @@
 import { env as public_env} from '$env/dynamic/public';
 import { redirect } from '@sveltejs/kit';
 
-export const load = async () => {
-    // todo
-}
-
 export const actions = {
     auth: async ({ request}) =>{
         const formdata = await request.formData();
