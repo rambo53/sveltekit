58ef29aaed0cb20984d0e79a65700fdbde628632
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-23 15:42:29 +0100

    Update +page.svelte

--- src/routes/authenticate/+page.svelte
+++ src/routes/authenticate/+page.svelte
@@ -1,6 +1,37 @@
-<h1>Authentification</h1>
+<script>
+    import { env as public_env} from '$env/dynamic/public'
 
-<form id="formAuth" method="post" action="?/auth">
+    const checkForm = async () => {
+        const form = document.getElementById('formAuth');
+        let formdata = new FormData(form);
+        const user = {
+            id :formdata.get("id"),
+            password:formdata.get("password")
+        }
+        
+        let r = await fetch(public_env.PUBLIC_URL_API+'authenticate', 
+        {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers:{
+                "Content-type":"application/json; charset=UTF-8",
+                "Accept":"application/json"
+            }
+        });
+        let data = await r.json();
+        if(data.status==200){
+            console.log("ok auth");
+            document.cookie = "token="+data.token;
+            document.cookie = "admin=true";
+            console.log(document.cookie)
+            form.reset();
+        }
+    }
+</script>
+
+<h1>Authenticate</h1>
+
+<form id="formAuth" method="post" on:submit|preventDefault={checkForm}>
     <label for="id">identifiant :</label>
     <br>
     <input name="id" type="texte">
