ed1543c755e397b3f90375963dbb6dcdda0d23e5
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-11-23 15:36:23 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,35 @@
+<script>
+	import { onMount } from 'svelte';
+    import publicRoutes from "$lib/data.json";
+
+    let userRole = Boolean();
+
+	onMount(async () => {
+        console.log(publicRoutes)
+        console.log(document.cookie)
+        let cookie = () =>{
+            let valCookie;
+            document.cookie.split(';').forEach(function(el){
+                let [key,value] = el.split('=');
+                if(key.trim() === 'admin'){
+                    valCookie = value
+                }
+            })
+            return valCookie
+        }
+        userRole = Boolean(cookie());
+	});
+</script>
+
 <nav>
     <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/settings">Settings</a>
-    <a href="/form">form</a>
+    {#if userRole}
+        <a href="/about">simple fetch</a>
+        <a href="/settings">multiple fetch</a>
+        <a href="/form">form</a>
+        <a href="/create">create</a>
+    {/if}
+    <a href="/authenticate">authenticate</a>
 </nav>
 
 <main>
